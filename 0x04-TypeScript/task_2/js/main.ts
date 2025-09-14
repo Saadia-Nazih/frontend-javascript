649669f421e63cd65d06c1c267360584d1c4907d
Saadia Nazih <sousounazih98@gmail.com>
2025-09-14 16:30:10 +0000

    Apply precise fixes for Task 2 checker

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,111 +1,56 @@
-
 // --- Interfacce ---
-
 interface DirectorInterface {
-
   workFromHome(): string;
-
   getCoffeeBreak(): string;
-
   workDirectorTasks(): string;
-
 }
-
-
 
 interface TeacherInterface {
-
   workFromHome(): string;
-
   getCoffeeBreak(): string;
-
   workTeacherTasks(): string;
-
 }
-
-
 
 // --- Classi ---
-
 class Director implements DirectorInterface {
-
   workFromHome = () => 'Working from home';
-
   getCoffeeBreak = () => 'Getting a coffee break';
-
   workDirectorTasks = () => 'Getting to director tasks';
-
 }
-
-
 
 class Teacher implements TeacherInterface {
-
   workFromHome = () => 'Impossibile lavorare da casa';
-
   getCoffeeBreak = () => 'Cannot have a break';
-
   workTeacherTasks = () => 'Getting to work';
-
 }
-
-
 
 // --- Funzione createEmployee ---
-
 function createEmployee(salary: number | string): Director | Teacher {
-
   if (typeof salary === 'number' && salary < 500) {
-
     return new Teacher();
-
   }
-
   return new Director();
-
 }
 
+// --- Funzioni richieste dal test (con le correzioni giuste) ---
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return 'workDirectorTasks' in employee;
+}
 
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
 
-// --- Tipi Avanzati Parte 2 ---
-
+// --- Tipo e funzione extra ---
 type Subjects = 'Math' | 'History';
 
-
-
-function isDirector(employee: Director | Teacher): employee is Director {
-
-  return 'workDirectorTasks' in employee;
-
-}
-
-
-
-function executeWork(employee: Director | Teacher) {
-
-  if (isDirector(employee)) {
-
-    console.log(employee.workDirectorTasks());
-
-  } else {
-
-    console.log(employee.workTeacherTasks());
-
-  }
-
-}
-
-
-
 function teachClass(todayClass: Subjects): string {
-
   if (todayClass === 'Math') {
-
     return 'Teaching Math';
-
   }
-
   return 'Teaching History';
-
 }
-
